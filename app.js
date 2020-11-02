54e386f761e853dbacd917e4aff4462ccb86dbad
Yi Long <ylong9@hawk.iit.edu>
2020-11-02 16:25:30 -0600

    added start scripts

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const express = require("express"),
 const indexRoutes = require("./routes/index");
 app.use(indexRoutes);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () =>{
     console.log("Listening on port ", port)
 })
