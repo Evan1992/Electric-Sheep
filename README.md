2c124f9de108c0e08926b17d641ef9775f278529
Evan1992 <280132947@qq.com>
2020-11-14 22:31:13 -0600

    added weather module

--- README.md
+++ README.md
@@ -1,10 +1,25 @@
 # Electric Sheep
-> This is my personal page, used for store and display some personal stuff.
 
-# Future Functions
+**Version 1.0.0**
+
+This is my personal page to store and display my favorite books, dramas, games, and etc. Also, there are links to some useful tools and web applications implemented by me.
+
+---
+
+## Contributors
+- Yi Long
+
+---
+
+## Future Functions
 * Weather
 * News
 * Message board
 * Leverage Location API to dynamically track current location
 * TimeLogger, recording the time spent for different tasks to improve the planning
 * Support multiple languages
+
+---
+
+## License & Copyright
+© Yi Long
--- app.js
+++ app.js
@@ -1,22 +1,25 @@
 const express = require("express"),
-      app     = express()
+      app     = express(),
+      request = require("request")
 
 /*
-Set path in order to use external files
-*/
+ * Set path in order to use external files
+ */
 app.use(express.static(__dirname+"/views"));
-app.use(express.static(__dirname+"/public/pictures/books"))
-app.use(express.static(__dirname+"/public/pictures/dramas"))
+app.use(express.static(__dirname+"/public/pictures/books"));
+app.use(express.static(__dirname+"/public/pictures/dramas"));
+app.use(express.static(__dirname+"/public/pictures/music"));
+app.use(express.static(__dirname+"/public/pictures/weather_icons"));
 
 /*
-Require routes
-*/
+ * Require routes
+ */
 const indexRoutes = require("./routes/index");
 app.use(indexRoutes);
 
 /*
-Connect to Server
-*/
+ * Connect to Server
+ */
 const port = process.env.PORT || 3000; 
 app.listen(port, () =>{
     console.log("Listening on port", port)
