08b28c393c1c3dea6df27596853b914ab21ad84a
Yi Long <280132947@qq.com>
2024-07-26 23:45:53 -0700

    Add data model for drama

---
+++ models/drama.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose")
+
+var DramaSchema = new mongoose.Schema({
+    name:        String,
+    director:    String,
+    year:        String,
+    cover:   {
+        img_data: Buffer,
+        contentType: String
+    },
+    recurrence:  Number,
+    stars:       Number,
+    lines:       Array,
+    comments:    Array,
+    haveWatched: Boolean,
+})
+
+module.exports = mongoose.model("Drama", DramaSchema);
