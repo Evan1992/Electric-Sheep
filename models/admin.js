20fa90b4a59533e7194f8673fe8c775ca1658306
Yi Long <280132947@qq.com>
2024-11-28 22:13:19 +1100

    Add Admin mongoose model to authenticate admin user

---
+++ models/admin.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose")
+
+var AdminSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true }, // Store hashed passwords
+})
+
+module.exports = mongoose.model("Admin", AdminSchema);
