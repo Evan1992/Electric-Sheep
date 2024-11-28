211978081bfe327a12393054c21f96ae8c58d496
Yi Long <280132947@qq.com>
2024-11-28 22:41:16 +1100

    Add a middleware to hash the password

--- models/admin.js
+++ models/admin.js
@@ -1,8 +1,25 @@
 const mongoose = require("mongoose")
+const bcrypt = require('bcrypt');
 
 var AdminSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true }, // Store hashed passwords
 })
+
+// Pre-save middleware to hash the password
+AdminSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        return next(); // Skip if password is not modified
+    }
+
+    try {
+        // Generate a salt and hash the password
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (err) {
+        next(err);
+    }
+});
 
 module.exports = mongoose.model("Admin", AdminSchema);
