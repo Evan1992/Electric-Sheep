c0d3b2d82b11136fec233083ef36d29c61b19d4e
Yi Long <280132947@qq.com>
2025-06-16 22:06:32 -0700

    Add data model for software

---
+++ models/software.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose")
+
+var SoftwareSchema = new mongoose.Schema({
+    name:        String,
+    company:     String,
+    year:        String,
+    intro:       String,
+    platforms:   Array,
+    cover:   {
+        img_data: Buffer,
+        contentType: String
+    },
+    stars:       Number,
+    itemType:    String
+})
+
+module.exports = mongoose.model("Software", SoftwareSchema);
